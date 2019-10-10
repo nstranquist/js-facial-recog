9eef3b508ee68beb2e1710fe2d76385e53512814
nicostran <nstranquist@gmail.com>
2019-10-10 14:45:29 -0500

    tracking face with canvas

--- main.js
+++ main.js
@@ -34,13 +34,22 @@ function startVideo() {
 }
 
 video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = {
+    width: video.width,
+    height: video.height
+  }
+  faceapi.matchDimensions(canvas, displaySize)  // more accurate detection with canvas
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces( // gets all faces inside of webcam image
       video,
       new faceapi.TinyFaceDetectorOptions()
     ).withFaceLandmarks()
       .withFaceExpressions()  // will determine expression of face
-
     console.log(detections)
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height) // clears entire canvas befor re-drawing
+    faceapi.draw.drawDetections(canvas, resizedDetections)
   }, 100)
 })
