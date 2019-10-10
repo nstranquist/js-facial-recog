9a428d884f596133cf5d29e26515f822d1e24bac
nicostran <nstranquist@gmail.com>
2019-10-10 14:16:53 -0500

    fix navigator.getUserMedia errors

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 const video = document.getElementById('video')
 
+
+
 function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
+  navigator.getUserMedia = (
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia
   )
+
+  if (typeof navigator.mediaDevices.getUserMedia === 'undefined') {
+    navigator.getUserMedia({
+      video: {}
+    },
+      stream => video.srcObject = stream,
+      err => console.error(err)
+    )
+  } else {
+    navigator.mediaDevices.getUserMedia({
+      video: {}
+    }).then(stream => video.srcObject = stream)
+      .catch(err => console.error(err));
+  }
+
+  //navigator.getUserMedia = (
+  //  { video: {} },
+  //  stream => video.srcObject = stream,
+  //  err => console.error(err)
+  //)
 }
 
 startVideo()
